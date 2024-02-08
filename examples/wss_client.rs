4ac72e59862f6e54d9459054b2fd7729e2208fad
Osteoporosis <16424085+Osteoporosis@users.noreply.github.com>
2024-02-09 00:52:06 +0900

    update the example

--- examples/wss_client.rs
+++ examples/wss_client.rs
@@ -15,92 +15,102 @@ struct Subscription {
 async fn subscribe(
     client: &mut client::Online,
     started_at: Instant,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let data = Subscription {
         method: "SUBSCRIBE".to_string(),
         params: vec!["btcusdt@bookTicker".to_string()],
         id: started_at.elapsed().as_nanos(),
     };
-    tokio::time::timeout(Duration::from_nanos(0), client.send_json(&data)).await??;
+    tokio::time::timeout(Duration::from_millis(0), client.send_json(&data)).await??;
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let started_at = Instant::now();
-    let url = "wss://data-stream.binance.vision:9443/ws/bakeusdt@bookTicker"; // the lowest volume
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
 
-    'reconnect_loop: loop {
-        let future = connect(url);
-        /*
-            alternative code for an example
-            make a Offline client and apply an intentional error raising setting before `connect`
-        */
-        // let mut client = client::Offline::new();
-        // client.set_max_message_size(64);
-        // let future = client.connect(url);
+    // the lowest volume example
+    let url = "wss://data-stream.binance.vision:9443/ws/bakeusdt@bookTicker";
 
-        let mut client: client::Online = match future.await {
-            Ok(client) => {
-                println!("conneted");
-                client
-            }
-            Err(e) => {
-                eprintln!("Reconnecting from an Error: {e:?}");
-                tokio::time::sleep(Duration::from_secs(10)).await;
-                continue;
-            }
-        };
+    let handle = runtime.spawn(async move {
+        'reconnect_loop: loop {
+            let future = connect(url);
+            /*
+                alternative code for an example:
+                    1. make a Offline client
+                    2. apply an intentional error raising setting before `connect`
+                    3. call `connect` to get a future
+            */
+            // let mut client = client::Offline::new();
+            // client.set_max_message_size(64);
+            // let future = client.connect(url);
 
-        // we can modify settings while running.
-        // without pong, this app stops in about 15 minutes.(by the API spec.)
-        client.set_auto_pong(false);
-
-        // add one more subscription here, or comment out below to see the timeouts
-        if let Err(e) = subscribe(&mut client, started_at).await {
-            eprintln!("Reconnecting from an Error: {e:?}");
-            let _ = client.send_close(&[]).await;
-            tokio::time::sleep(Duration::from_secs(10)).await;
-            continue;
-        };
-
-        // message processing loop
-        loop {
-            let message = if let Ok(result) =
-                tokio::time::timeout(Duration::from_secs(1), client.receive_frame()).await
-            {
-                match result {
-                    Ok(message) => message,
-                    Err(e) => {
-                        eprintln!("Reconnecting from an Error: {e:?}");
-                        let _ = client.send_close(&[]).await;
-                        break; // break the message loop then reconnect
-                    }
+            let mut client: client::Online = match future.await {
+                Ok(client) => {
+                    println!("conneted");
+                    client
                 }
-            } else {
-                println!("timeout");
+                Err(e) => {
+                    eprintln!("Reconnecting from an Error: {e:?}");
+                    tokio::time::sleep(Duration::from_secs(10)).await;
+                    continue;
+                }
+            };
+
+            // we can modify settings while running.
+            // without pong, this app stops in about 15 minutes.(by the binance API spec.)
+            client.set_auto_pong(false);
+
+            // add one more example subscription here after connect
+            if let Err(e) = subscribe(&mut client, started_at).await {
+                eprintln!("Reconnecting from an Error: {e:?}");
+                let _ = client.send_close(&[]).await;
+                tokio::time::sleep(Duration::from_secs(10)).await;
                 continue;
             };
 
-            match message.opcode {
-                OpCode::Text => {
-                    let payload = match String::from_utf8(message.payload.to_vec()) {
-                        Ok(payload) => payload,
+            // message processing loop
+            loop {
+                let message = if let Ok(result) =
+                    tokio::time::timeout(Duration::from_millis(100), client.receive_frame()).await
+                {
+                    match result {
+                        Ok(message) => message,
                         Err(e) => {
                             eprintln!("Reconnecting from an Error: {e:?}");
                             let _ = client.send_close(&[]).await;
                             break; // break the message loop then reconnect
                         }
-                    };
-                    println!("{payload}");
+                    }
+                } else {
+                    println!("timeout");
+                    continue;
+                };
+
+                match message.opcode {
+                    OpCode::Text => {
+                        let payload = match simdutf8::basic::from_utf8(message.payload.as_ref()) {
+                            Ok(payload) => payload,
+                            Err(e) => {
+                                eprintln!("Reconnecting from an Error: {e:?}");
+                                let _ = client.send_close(&[]).await;
+                                break; // break the message loop then reconnect
+                            }
+                        };
+                        println!("{payload}");
+                    }
+                    OpCode::Close => {
+                        println!("{:?}", String::from_utf8_lossy(message.payload.as_ref()));
+                        break 'reconnect_loop;
+                    }
+                    _ => {}
                 }
-                OpCode::Close => {
-                    println!("{:?}", String::from_utf8_lossy(message.payload.as_ref()));
-                    break 'reconnect_loop;
-                }
-                _ => {}
             }
         }
-    }
+    });
+    runtime.block_on(handle)?;
     Ok(())
 }
